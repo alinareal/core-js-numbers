6f66a2a76dd194af29ef05f36c5be33c28dfafad
Alina Layevskaya <alina.layevskaya@gmail.com>
2023-11-13 14:23:39 +0300

    feat: implement function getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -281,8 +281,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sumToN = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sumToN += i;
+  }
+  return sumToN;
 }
 
 /**
